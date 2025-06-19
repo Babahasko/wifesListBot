57ca8b5ac1549497c959ddeda95128b2b56ddf53
Babahasko <zakutni4ek@gmail.com>
2025-06-20 00:29:04 +0300

    add inline kb for lists

--- internal/shop/inline_keyboard.go
+++ internal/shop/inline_keyboard.go
@@ -1,45 +1,45 @@
 package shop
 
-// import (
-// 	"fmt"
+import (
+	"fmt"
 
-// 	"shopping_bot/pkg/callback"
+	"shopping_bot/pkg/callback"
 
-// 	"github.com/PaulSonOfLars/gotgbot/v2"
-// )
+	"github.com/PaulSonOfLars/gotgbot/v2"
+)
 
 // TODO: Здесь будет клавиатура формирования списка покупок
 
-// func getCategoriesKeyboard(categories []string) (*gotgbot.InlineKeyboardMarkup, error) {
-// 	var ButtonsPerRow = 3
-// 	var rows [][]gotgbot.InlineKeyboardButton
-// 	var buttons []gotgbot.InlineKeyboardButton
-// 	for _, category := range categories {
+func getListsKeyboard(lists []string) (*gotgbot.InlineKeyboardMarkup, error) {
+	var ButtonsPerRow = 1
+	var rows [][]gotgbot.InlineKeyboardButton
+	var buttons []gotgbot.InlineKeyboardButton
+	for _, list := range lists {
 
-// 		//Callback
-// 		callbackStr, err := callback.PackCallback(&CategoryCallback{
-// 			Name: category,
-// 		})
-// 		if err != nil {
-// 			return nil, fmt.Errorf("failed to pack callback: %v", err)
-// 		}
+		//Callback
+		callbackStr, err := callback.PackCallback(&ListCallback{
+			Name: list,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to pack callback: %v", err)
+		}
 
-// 		buttons = append(buttons, gotgbot.InlineKeyboardButton{
-// 			Text:         category,
-// 			CallbackData: callbackStr,
-// 		})
+		buttons = append(buttons, gotgbot.InlineKeyboardButton{
+			Text:         list,
+			CallbackData: callbackStr,
+		})
 
-// 		if len(buttons) == ButtonsPerRow {
-// 			rows = append(rows, buttons)
-// 			buttons = []gotgbot.InlineKeyboardButton{}
-// 		}
-// 	}
+		if len(buttons) == ButtonsPerRow {
+			rows = append(rows, buttons)
+			buttons = []gotgbot.InlineKeyboardButton{}
+		}
+	}
 
-// 	if len(buttons) > 0 {
-// 		rows = append(rows, buttons)
-// 	}
+	if len(buttons) > 0 {
+		rows = append(rows, buttons)
+	}
 
-// 	return &gotgbot.InlineKeyboardMarkup{
-// 		InlineKeyboard: rows,
-// 	}, nil
-// }
+	return &gotgbot.InlineKeyboardMarkup{
+		InlineKeyboard: rows,
+	}, nil
+}
