bb1820708c027e6f47e05d105f008f3c4affa2b4
Babahasko <zakutni4ek@gmail.com>
2025-06-20 01:34:30 +0300

    add button file

---
+++ internal/shop/buttons.go
@@ -0,0 +1,12 @@
+package shop
+
+const (
+	ButtonAddPurchase = "✍️Сформировать список покупок"
+	ButtonViewList    = "📋Открыть список покупок"
+	ButtonFinishList  = "✅Завершить список"
+	ButtonCancel      = "❌Отмена"
+	ButtonBack        = "⬅️Назад"
+	ButtonBackToLists = "⬅️Назад к спискам"
+	ButtonEmptyList   = "🫗Список пуст"
+	ButtonClearList = "🧹Очистить Список"
+)
