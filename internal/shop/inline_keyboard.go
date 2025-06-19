fee4ff97dc54d253b78fc1da83147df1bb80d80f
Babahasko <zakutni4ek@gmail.com>
2025-06-20 01:40:44 +0300

    add items keyboard

--- internal/shop/inline_keyboard.go
+++ internal/shop/inline_keyboard.go
@@ -43,3 +43,49 @@ func getListsKeyboard(lists []string) (*gotgbot.InlineKeyboardMarkup, error) {
 		InlineKeyboard: rows,
 	}, nil
 }
+
+func getItemsKeyboard(listName string, items []string) (*gotgbot.InlineKeyboardMarkup, error) {
+    if len(items) == 0 {
+        return &gotgbot.InlineKeyboardMarkup{
+            InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+                {
+                    {Text: ButtonEmptyList, CallbackData: "no_items"},
+                },
+                {
+                    {Text: ButtonBack, CallbackData: "back_to_lists"},
+                },
+            },
+        }, nil
+    }
+
+    // Создаем кнопки для каждой покупки
+    var keyboard [][]gotgbot.InlineKeyboardButton
+    for _, item := range items {
+        // Здесь можно добавить callback данные для каждой покупки
+        // Например, чтобы можно было отметить как купленное
+		callbackStr, err := callback.PackCallback(&ItemCallback{
+			ListName: listName,
+			Name: item,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to pack callback")
+		}
+        row := []gotgbot.InlineKeyboardButton{
+            {
+                Text:         item,
+                CallbackData: callbackStr,
+            },
+        }
+        keyboard = append(keyboard, row)
+    }
+
+    // Добавляем кнопку "Назад"
+    keyboard = append(keyboard, []gotgbot.InlineKeyboardButton{
+        {Text: ButtonBackToLists, CallbackData: "back_to_lists"},
+		{Text: ButtonClearList, CallbackData: "clear_list"},
+    })
+
+    return &gotgbot.InlineKeyboardMarkup{
+        InlineKeyboard: keyboard,
+    }, nil
+}
