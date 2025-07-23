21adc3faa9256105c3ab9e786a8f52f5f41565ba
Babahasko <zakutni4ek@gmail.com>
2025-07-24 00:05:21 +0300

    rollback

--- internal/repository/models.go
+++ internal/repository/models.go
@@ -1,21 +1,16 @@
 package repository
 
-import "gorm.io/gorm"
-
 type UserState struct {
-	gorm.Model
 	CurrentList string `json:"state"` //текущий лист покупок редактируемый пользователем
 }
 
 type ShoppingList struct {
-	gorm.Model
 	Name  string          `json:"name"`
-	Items []*ShoppingItem `json:"items" gorm:"foreignKey:ShoppingListID"`
+	Items []*ShoppingItem `json:"items"`
 }
 
 type ShoppingItem struct {
-	gorm.Model
-	ShoppingListID uint `json:"shopping_list_id" gorm:"index"`
+	ShoppingListID uint `json:"shopping_list_id"`
 	ListName string `json:"list_name"`
 	Name     string `json:"item_name"`
 	Checked  bool   `json:"checked"`
