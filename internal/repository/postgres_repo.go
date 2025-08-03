fc80ffbffcae13e36b4700b747dae30573653559
Babahasko <zakutni4ek@gmail.com>
2025-08-04 01:07:58 +0300

    add shopping list methods

--- internal/repository/postgres_repo.go
+++ internal/repository/postgres_repo.go
@@ -20,17 +20,17 @@ type GormUserState struct {
 
 type GormShoppingList struct {
 	gorm.Model
-	UserID int64 `gorm:"index"`
-	Name   string
+	UserID int64               `gorm:"index"`
+	Name   string              `gorm:"not null"`
 	Items  []*GormShoppingItem `gorm:"foreignKey:ShoppingListID"`
 }
 
 type GormShoppingItem struct {
 	gorm.Model
-	ShoppingListID uint
-	ListName       string
-	Name           string
-	Checked        bool
+	ShoppingListID uint   `gorm:"not null"`
+	ListName       string `gorm:"not null"`
+	Name           string `gorm:"not null"`
+	Checked        bool   `gorm:"default:false"`
 }
 
 func NewPostgresShoppingRepository(db *gorm.DB) *PostgresShoppingRepository {
@@ -41,16 +41,16 @@ func (r *PostgresShoppingRepository) GetUserState(userID int64) (*models.UserSta
 	var gormUserState GormUserState
 	result := r.db.First(&gormUserState, "user_id = ?", userID)
 	if result.Error != nil {
-        if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-            return nil, ErrNoState
-        }
-        return nil, result.Error
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil, ErrNoState
+		}
+		return nil, result.Error
 	}
 	return r.convertUserStateToModel(&gormUserState), nil
 }
 
 func (r *PostgresShoppingRepository) SetUserState(userID int64, state *models.UserState) error {
-    gormUserState := r.convertUserStateToGormModel(userID, state)
+	gormUserState := r.convertUserStateToGormModel(userID, state)
 	result := r.db.Create(&gormUserState)
 	if result.Error != nil {
 		return result.Error
@@ -59,12 +59,56 @@ func (r *PostgresShoppingRepository) SetUserState(userID int64, state *models.Us
 }
 
 func (r *PostgresShoppingRepository) AddShoppingList(userID int64, listName string) error {
+	// Проверяем, что список существует
+	var existingList GormShoppingList
+	result := r.db.Where("user_id = ? AND name = ?", userID, listName).First(&existingList)
+
+	if result.Error == nil {
+		return ErrListExists
+	}
+
+	if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return result.Error
+	}
+
+	newList := GormShoppingList{
+		UserID: userID,
+		Name:   listName,
+		Items:  make([]*GormShoppingItem, 0),
+	}
+
+	result = r.db.Create(&newList)
+	if result.Error != nil {
+		return result.Error
+	}
 	return nil
 }
+
 func (r *PostgresShoppingRepository) GetUserLists(userID int64) (map[string]*models.ShoppingList, error) {
-	return nil, nil
+	var gormLists []GormShoppingList
+	result := r.db.Where("user_id = ?", userID).Preload("Items").Find(&gormLists)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	// Преобразуем в map[string]*models.ShoppingList
+	userLists := make(map[string]*models.ShoppingList)
+
+	for _, gormList := range gormLists {
+		shoppingList := r.convertGormListToModel(&gormList)
+		userLists[gormList.Name] = shoppingList
+	}
+	return userLists, nil
 }
 func (r *PostgresShoppingRepository) DeleteList(userID int64, listName string) error {
+	result := r.db.Where("user_id = ? AND name = ?", userID, listName).Delete(&GormShoppingList{})
+	if result.Error != nil {
+		return result.Error
+	}
+
+	if result.RowsAffected == 0 {
+		return ErrListExists
+	}
+	
 	return nil
 }
 
@@ -81,16 +125,31 @@ func (r *PostgresShoppingRepository) DeleteMarkedItems(userID int64, listName st
 	return nil
 }
 
-
-//function for converting from gorm model to standart model
+// function for converting from gorm model to standart model
 func (r *PostgresShoppingRepository) convertUserStateToModel(gormUS *GormUserState) *models.UserState {
-    return &models.UserState{
-        CurrentList: gormUS.CurrentList,
-    }
+	return &models.UserState{
+		CurrentList: gormUS.CurrentList,
+	}
 }
 func (r *PostgresShoppingRepository) convertUserStateToGormModel(userid int64, userState *models.UserState) *GormUserState {
-    return &GormUserState{
-        UserID: userid,
+	return &GormUserState{
+		UserID:      userid,
 		CurrentList: userState.CurrentList,
-    }
+	}
+}
+
+func (r *PostgresShoppingRepository) convertGormListToModel(gormList *GormShoppingList) *models.ShoppingList {
+	items := make([]*models.ShoppingItem, len(gormList.Items))
+	for i, gormItem := range gormList.Items {
+		items[i] = &models.ShoppingItem{
+			ShoppingListID: gormItem.ShoppingListID,
+			ListName: gormItem.ListName,
+			Name: gormItem.Name,
+			Checked: gormItem.Checked,
+		}
+	}
+	return &models.ShoppingList{
+		Name: gormList.Name,
+		Items: items,
+	}
 }
--- internal/repository/postgres_repo_test.go
+++ internal/repository/postgres_repo_test.go
@@ -33,3 +33,42 @@ func TestUserStateOperations(t *testing.T) {
 		assert.ErrorIs(t, err, repository.ErrNoState)
 	})
 }
+
+func TestShoppingListOperations(t *testing.T) {
+	db := integration.SetupTestDB(t)
+	repo := repository.NewPostgresShoppingRepository(db)
+
+	t.Run("Add get and delete ShoppingList", func(t *testing.T) {
+		userID := int64(489)
+		listName1 := "Еда"
+		listName2 := "Авто"
+		listName3 := "Любимой"
+
+		err := repo.AddShoppingList(userID, listName1)
+		require.NoError(t, err)
+
+		err = repo.AddShoppingList(userID, listName2)
+		require.NoError(t, err)
+
+		err = repo.AddShoppingList(userID, listName3)
+		require.NoError(t, err)
+
+		userLists, err := repo.GetUserLists(userID)
+		require.NoError(t, err)
+
+		// Проверяем, что список не пустой
+		assert.NotEmpty(t, userLists)
+
+		// Проверяем, что в списке есть элемент с нужным именем
+		assert.Contains(t, userLists, listName1)
+
+		// Удаляем список
+		err = repo.DeleteList(userID, listName1)
+		require.NoError(t, err)
+
+		// Проверяем, что список удалён
+		userLists, err = repo.GetUserLists(userID)
+		require.NoError(t, err)
+		assert.NotContains(t, userLists, listName1)
+	})
+}
